c0599e632edb34c8abff3ea3d2f85932b3add335
Matthew Beckett <matt@beckett.cloud>
2022-07-07 01:13:03 +0100

    Replica entrypoint changes for stopping file creation in root on workers

--- multipaper-server-entrypoint.sh
+++ multipaper-server-entrypoint.sh
@@ -6,4 +6,5 @@ fi
 
 
 echo "Starting multipaper-server instance with name $MULTIPAPER_BUNGEECORD_NAME"
-java -Dcom.mojang.eula.agree="$ACCEPT_EULA" -DbungeecordName="$MULTIPAPER_BUNGEECORD_NAME" -DmultipaperMasterAddress="$MULTIPAPER_MASTER_ADDRESS" -Dproperties.view-distance="$VIEW_DISTANCE" -jar /opt/multipaper-server/multipaper-server.jar
+cd /opt/multipaper-server
+java -Dcom.mojang.eula.agree="$ACCEPT_EULA" -DbungeecordName="$MULTIPAPER_BUNGEECORD_NAME" -DmultipaperMasterAddress="$MULTIPAPER_MASTER_ADDRESS" -Dproperties.view-distance="$VIEW_DISTANCE" -jar ./multipaper-server.jar
